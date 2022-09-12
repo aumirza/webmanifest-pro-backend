420f45eab746ea26072bead42dc44710e800bf59
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2022-09-12 23:08:17 +0530

    few changes

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,6 @@
 
+import os
+from zipfile import ZipFile
 from backend.app import ALLOWED_IMAGE_EXTENSIONS
 
 
@@ -15,3 +17,11 @@ def get_image_path(filename, dir):
 
 def get_file_path(filename, dir):
     return os.path.join(os.path.dirname(__file__), app.config['TEMP_ZIP_FOLDER'], dir, filename)
+
+
+def make_zipfile(output_filename, source_dir):
+    with ZipFile(output_filename, 'w') as zipObj:
+        for folderName, subfolders, filenames in os.walk(source_dir):
+            for filename in filenames:
+                filePath = get_file_path(filename, folderName)
+                zipObj.write(filePath, os.path.basename(filePath))
